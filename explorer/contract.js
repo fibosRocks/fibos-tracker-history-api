574f8c32564bd5188a340432e65ed8e3e1129d12
qiushaoxi <qiushaoxi@163.com>
2019-12-22 23:18:03 +0800

    fix

--- explorer/contract.js
+++ explorer/contract.js
@@ -11,50 +11,40 @@ function getEndPoint() {
 
 module.exports = (memory, db) => {
     function cacheContracts() {
-        db.all(SQL`SELECT DISTINCT account FROM fibos_contracts`).then(contracts => {
-            contracts.forEach(contract => {
+        db.all(SQL`SELECT DISTINCT account FROM fibos_contracts`).then(async contracts => {
+            for (const contract of contracts) {
                 const { account } = contract
-
                 const httpEndPoint = getEndPoint();
-
-                const abiPromise = axios.post(`${httpEndPoint}/v1/chain/get_abi`, {
+                const abiRes = await axios.post(`${httpEndPoint}/v1/chain/get_abi`, {
                     "account_name": account
-                }).then(res => {
-                    const { data } = res;
-                    return data;
-                }).catch(err => {
-                    console.error(err)
                 })
+                const abi = abiRes.data
 
-                const hashPromise = axios.post(`${httpEndPoint}/v1/chain/get_code_hash`, {
+                const hashRes = await axios.post(`${httpEndPoint}/v1/chain/get_code_hash`, {
                     "account_name": account
-                }).then(res => {
-                    const { data } = res;
-                    return data.code_hash;
-                }).catch(err => {
-                    console.error(err)
                 })
+                const hash = hashRes.data.code_hash;
 
-                const countPromise = db.get(SQL`SELECT COUNT(DISTINCT trx_id)  as tx_count FROM fibos_actions WHERE contract=${account}`).then(res => {
-                    return res.tx_count
-                })
+                const countRes = await db.get(SQL`SELECT COUNT(DISTINCT trx_id)  as tx_count FROM fibos_actions WHERE contract=${account}`)
+                const count = countRes.tx_count
 
-                Promise.all([abiPromise, countPromise, hashPromise]).then(([abi, count, hash]) => {
-                    let actions = [];
-                    if (abi.abi) {
-                        abi.abi.actions.forEach(action => {
-                            actions.push(action.name);
-                        });
-                    }
-                    let obj = {
-                        contract: account,
-                        count, actions, abi, hash,
-                        is_token: (actions.indexOf("transfer") != -1)/* ,
+                let actions = [];
+                if (abi.abi) {
+                    abi.abi.actions.forEach(action => {
+                        actions.push(action.name);
+                    });
+                }
+                let obj = {
+                    contract: account,
+                    count, actions, abi, hash,
+                    is_token: (actions.indexOf("transfer") != -1)/* ,
                                     is_open: openContracts.includes(contract) */
-                    }
-                    memory.hset("contracts", obj.contract, obj);
-                })
-            })
+                }
+                memory.hset("contracts", obj.contract, obj);
+
+            }
+        }).catch(err => {
+            console.error(err)
         })
     }
 
